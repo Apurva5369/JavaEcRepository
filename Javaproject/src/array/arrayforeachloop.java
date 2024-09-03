5c81b29f25027afa3329b3f99063d283b050e4df
Apurva5369 <apurva.kanavade369@gmail.com>
2024-09-03 19:14:49 +0530

    Add Java Program

---
+++ Javaproject/src/array/arrayforeachloop.java
@@ -0,0 +1,15 @@
+package array;
+
+public class arrayforeachloop 
+{
+public static void main(String args[])
+{
+	int a[]= {10,20,30,70,49,96,22,99,369};
+	
+	for(int b:a)
+	{
+		System.out.println(b);
+	}
+		
+}
+}
